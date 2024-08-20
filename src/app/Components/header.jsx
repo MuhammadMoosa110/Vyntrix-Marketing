23ea0d7f6fc9f0077bf535a6569f7f87f4fd2b67
MuhammadMoosa110 <moosay50000@gmail.com>
2024-08-20 19:04:22 +0500

    first commit

---
+++ src/app/Components/header.jsx
@@ -0,0 +1,15 @@
+'use client'
+import Style from '@/app/Components/header.css'
+function Header() {
+    return ( 
+        <>
+        <div className='HeaderWrapper'> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="55" height="56" fill="#D5ED9F" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+</svg>
+</div>
+        </>
+     );
+}
+
+export default Header;
