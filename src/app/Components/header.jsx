d8b80e20d52a718f8ef81808bf9b6d0c5db2bdab
MuhammadMoosa110 <moosay50000@gmail.com>
2024-08-27 19:54:21 +0500

    first commit

--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -13,7 +13,7 @@ function Header() {
         <div className='insideMenu'>
 
           <ul className={click ? 'open' : ' '}>
-            <Link href='/' className='cut' onClick={() => { setClick(!click) }}>     <svg xmlns="http://www.w3.org/2000/svg" width="1" height="10" fill="#fafafa" class="bi bi-x-lg" viewBox="0 0 16 16">
+            <Link href='' className='cut' onClick={() => { setClick(!click) }}>     <svg xmlns="http://www.w3.org/2000/svg" width="1" height="10" fill="#fafafa" class="bi bi-x-lg" viewBox="0 0 16 16">
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
             </svg></Link>
 
