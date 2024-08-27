19c59e8f7d5dd02d657d583e44a36e2e9e452c0b
MuhammadMoosa110 <moosay50000@gmail.com>
2024-08-27 19:47:44 +0500

    first commit

--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -6,16 +6,16 @@ import { useState } from 'react';
 function Header() {
   const [click, setClick] = useState(false)
   const [cut, setCut] = useState(false)
-   
+
   return (
     <>
       <div className='HeaderWrapper'>
         <div className='insideMenu'>
-           
-          <ul className= {  click ? 'open' : ' '  }>
-      <Link href='/' onClick={ () =>{setClick(!click)} }>     <svg xmlns="http://www.w3.org/2000/svg" width="02" height="10" fill="#fafafa" class="bi bi-x-lg" viewBox="0 0 16 16">
-  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-</svg></Link>
+
+          <ul className={click ? 'open' : ' '}>
+            <Link href='/' className='cut' onClick={() => { setClick(!click) }}>     <svg xmlns="http://www.w3.org/2000/svg" width="1" height="10" fill="#fafafa" class="bi bi-x-lg" viewBox="0 0 16 16">
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+            </svg></Link>
 
             <li><Link href={' /'} >Home</Link></li>
             <li><Link href={'/About'} >About</Link></li>
@@ -23,15 +23,15 @@ function Header() {
             <li><Link href={'/Contact'} >Contact</Link></li>
 
           </ul>
-           
-          <Link href=' ' onClick={ () =>{setClick(!click)} }  >  <svg xmlns="http://www.w3.org/2000/svg" width="55" height="56" fill="#FF9100" class="bi bi-list" viewBox="0 0 16 16">
+
+          <Link className='op' href=' ' onClick={() => { setClick(!click) }}  >  <svg xmlns="http://www.w3.org/2000/svg" width="55" height="56" fill="#FF9100" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-          </svg></Link>  
-        
-        
+          </svg></Link>
+
+
         </div>
-        
-           
+
+
 
       </div>
     </>
